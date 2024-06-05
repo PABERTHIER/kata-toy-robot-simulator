aec5fa14b9426e49958e1aacea1177a8fe13eb25
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2024-06-05 16:07:42 +0200

    Implementing the commands reader

--- KataToyRobotSimulator/KataToyRobotSimulator/Program.cs
+++ KataToyRobotSimulator/KataToyRobotSimulator/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace KataToyRobotSimulator;
 
-Console.WriteLine("Hello, World!");
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Boundaries boundaries = new() { MinPosition = new Position { X = 0, Y = 0 }, MaxPosition = new Position { X = 5, Y = 5 } };
+        MotionTable motionTable = new (boundaries);
+        ToyRobot toyRobot = new();
+        MotionCommands motionCommands = new (motionTable, toyRobot);
+
+        List<string> commands = new();
+
+        if (args.Length > 0)
+        {
+            string filePath = args[0];
+            commands.AddRange(File.ReadAllLines(filePath));
+        }
+        else
+        {
+            while (Console.ReadLine() is { } line && line != "")
+            {
+                commands.Add(line);
+            }
+        }
+
+        motionCommands.ProcessCommands(commands);
+    }
+}
